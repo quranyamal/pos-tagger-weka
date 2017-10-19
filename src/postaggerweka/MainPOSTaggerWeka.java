d068f2244c8a2cd1afdd9282c374f9060c094542
drestanto <dyas@live.com>
2017-10-19 16:57:37 +0700

    oov handle

--- src/postaggerweka/MainPOSTaggerWeka.java
+++ src/postaggerweka/MainPOSTaggerWeka.java
@@ -1,9 +1,14 @@
 package postaggerweka;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
+import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -41,7 +46,20 @@ public class MainPOSTaggerWeka {
             System.out.println(nominalData.firstInstance()+"\n");
             
             // save ARFF
-            ptw.saveArff(nominalData, "test.arff"); // debugging purposes
+            ptw.saveArff(nominalData, "test.arff");
+            
+            // make array words
+            ArrayList<String> words = new ArrayList<>();
+            Attribute attr = nominalData.attribute(0);
+            String atrtibuteWord = attr.toString();
+            String[] split = atrtibuteWord.split(" ");
+            String[] splitSplit = split[2].split(",");
+            words.add(splitSplit[0].substring(1));
+            for (int i = 1; i<splitSplit.length-1;i++) {
+                words.add(splitSplit[i]);
+            }
+            String last = splitSplit[splitSplit.length-1];
+            words.add(last.substring(0,last.length()-1));
             
             // Build J48 classifier model
             J48 tree = new J48();
@@ -60,16 +78,20 @@ public class MainPOSTaggerWeka {
             String postag;
             String postagTwoBefore = "UNKNOWN2";
             String postagBefore = "UNKNOWN1";
+            Instance ins = nominalData.get(0);
             for (String s : splited) {
-                Instance ins = nominalData.get(0);
-                ins.setValue(0, s);
                 ins.setValue(1, postagTwoBefore);
                 ins.setValue(2, postagBefore);
-                ins.setValue(3, "NOUN");
+                if (words.contains(s))
+                    ins.setValue(0, s);
+                else {
+                    System.out.println("!!! OOV (Out of Vocabulary) : " + s + " !!!");
+                    ins.setValue(0, "dan");
+                }
+                
                 int indeks = (int) tree.classifyInstance(ins);
                 postag = nominalData.classAttribute().value(indeks);
                 System.out.println("Pos Tag untuk \""+s+"\" : " + postag);
-                
                 postagTwoBefore = postagBefore;
                 postagBefore = postag;
             }            
